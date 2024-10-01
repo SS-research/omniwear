69f3973c3cea4f898a56d52fb527cb3e5e1ebddc
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-01 14:14:35 +0200

    feat(api): add swagger deps + setup swagger ui

--- omniwear-api/src/main.ts
+++ omniwear-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('omniwear API')
+    .setDescription('omniwear API')
+    .setVersion('1.0')
+    .addTag('omniwear-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
