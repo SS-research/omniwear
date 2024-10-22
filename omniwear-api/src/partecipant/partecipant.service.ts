c04f525846a5bf8f72440d8b8d1ee304c430aecc
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-22 14:53:50 +0200

    feat(api/partecipant): checks if the participant id already exists and throws an error if it does not exist

--- omniwear-api/src/partecipant/partecipant.service.ts
+++ omniwear-api/src/partecipant/partecipant.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePartecipantDto } from './dto/create-partecipant.dto';
 import { UpdatePartecipantDto } from './dto/update-partecipant.dto';
 import { PrismaService } from '@app/prisma/prisma.service';
@@ -8,6 +12,21 @@ export class PartecipantService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createPartecipantDto: CreatePartecipantDto) {
+    const { partecipant_id } = createPartecipantDto;
+
+    // If partecipant_id is provided, check if it already exists
+    if (partecipant_id) {
+      const existingPartecipant = await this.prisma.partecipant.findUnique({
+        where: { partecipant_id },
+      });
+
+      if (existingPartecipant) {
+        // If the participant already exists, throw a conflict exception
+        throw new ConflictException(
+          `Partecipant with ID: ${partecipant_id} already exists`,
+        );
+      }
+    }
     return await this.prisma.partecipant.create({ data: createPartecipantDto });
   }
 
